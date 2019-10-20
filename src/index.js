5e177b9c9ed3e25cd3b0bf9d5df6d800e8197c67
sahan <sahandealmeida>
2019-10-20 17:54:14 -0400

    basic 2 player game working

--- src/index.js
+++ src/index.js
@@ -1,30 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
- 	constructor(props) {
-		super(props);
-		this.state = { 
-			value: null 
-		};
-	}
-
-  render() {
-    return (
-      <button className="square" onClick={()=>{alert("howdie");}}>
-        {this.props.value}
-      </button>
-    );
-  }
+//class Square extends React.Component {
+function Square(props) {  
+	return (
+		<button className="square" onClick={() => {props.onClick()}}>
+			{props.value}
+		</button>
+	);
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  	static  X = 'X';
+	static  O = 'O';
+	constructor(props) {
+		super(props)
+		this.state = {
+			squares: Array(9).fill(null), 
+			xIsNext: true,
+		};
+	}
+
+	handleClick(i) {
+		const squares = this.state.squares.slice();
+		if(this.calculateWinner(squares) || squares[i]){
+			return;
+		}
+		
+		squares[i] = this.getNext(); 
+		this.setState({
+			squares: squares,
+			xIsNext: !this.state.xIsNext
+		});
+	}
+
+	getNext() {
+		return this.state.xIsNext ? Board.X : Board.O;
+	}
+	
+	calculateWinner(squares) {
+		const lines = [
+		    [0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+		
+		for (let i = 0; i < lines.length; i++) {
+		    const [a, b, c] = lines[i];
+			    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				      return squares[a];
+				}
+		}
+		return null;
+	}
+
+	renderSquare(i) {
+    return (
+			<Square 
+				value={this.state.squares[i]} 
+				onClick={()=> this.handleClick(i)}
+			/>
+		);
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = this.calculateWinner(this.state.squares);
+	let status;
+	
+	if(winner) {
+		status= "Winner: " + winner;
+	}
+	else {
+ 		status = 'Next player: ' + this.getNext();
+	}
 
     return (
       <div>
